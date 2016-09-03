4f6259cf89544d37768d11b371846b758f97d124
Alexandru Panturu <alexandru.panturu@emag.ro>
2016-09-03 23:12:24 +0300

    read categories from api

--- services/response/categoryMenuResponse.js
+++ services/response/categoryMenuResponse.js
@@ -12,21 +12,7 @@ CategoryMenuResponse.prototype = {
                     template_type: "button",
                     text: "What do you want to do next?",
                     buttons: [
-                        {
-                            type: "postback",
-                            title: "Pizza",
-                            payload: "category_1"
-                        },
-                        {
-                            type: "postback",
-                            title: "Paste",
-                            payload: "category_2"
-                        },
-                        {
-                            type: "postback",
-                            title: "Ciorbe",
-                            payload: "category_3"
-                        }
+                      
                     ]
                 }
             }
