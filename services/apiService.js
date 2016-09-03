e203c58c97054e6ab2b1496cdb07d9655e86717c
Alexandru Panturu <alexandru.panturu@emag.ro>
2016-09-03 21:10:04 +0300

    add api service structure with 2 api calls

--- services/apiService.js
+++ services/apiService.js
@@ -1,33 +1,41 @@
 var request = require('request');
 
-const API_HOST = 'https://lapachet-bot.herokuapp.com';
+const API_HOST = process.env.UI_API_URL;
+const PRODUCTS_URL = '/products/{date}';
+const MENU_URL = '/menus/{date}';
+
+const HTTP_REQUEST_GET = 'GET';
+const HTTP_REQUEST_POST = 'POST';
 
 function ApiService() {
 }
 
 ApiService.prototype = {
     getProductsByDate: function (date) {
+        var url = PRODUCTS_URL.replace('{data}', date);
 
+        return this.request(url, HTTP_REQUEST_GET);
     },
     getDaliyMenusByDate: function (date) {
+        var url = MENU_URL.replace('{data}', date);
 
+        return this.request(url, HTTP_REQUEST_GET);
     },
     getUrl: function (url) {
         return API_HOST + url;
     },
     request: function (url, method) {
         var options = {
-            url: this.getUrl(url),
             port: 80,
             method: method
         };
-        request(this.getUrl(url), options, this.processResponse(error, response, body));
-    },
-    processResponse: function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-            console.log(body);
-            return body;
-        }
+        request(this.getUrl(url), options, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                return body;
+            } else {
+                console.log(error);
+            }
+        });
     }
 };
 
