287031d82a8fedd5ac32742aecb07fef14048855
Alexandru Panturu <alexandru.panturu@emag.ro>
2016-09-03 16:05:18 +0300

    merge and refactor

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
-
 /* GET webhook page. */
 router.get('/', function (req, res) {
-    var Webhook = require('../service/webhookService');
+    var Webhook = require('../services/webhookService');
     var hook = new Webhook();
 
     res.send(hook.verify(req));
