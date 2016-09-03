4de2db36652af103163dc9219517690e7a9824eb
Alexandru Panturu <alexandru.panturu@emag.ro>
2016-09-03 15:22:59 +0300

    .

--- routes/webhook.js
+++ routes/webhook.js
@@ -5,8 +5,11 @@ const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
 /* GET webhook page. */
 router.get('/', function (req, res, next) {
-    var Webhook = require('./webhook');
-    res.send(Webhook.verify(req, VERIFY_TOKEN));
+    if (req.query['hub.verify_token'] === VERIFY_TOKEN) {
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.send('Invalid verify token');
+    }
 });
 
 module.exports = router;
