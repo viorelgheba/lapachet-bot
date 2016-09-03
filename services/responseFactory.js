42b2b8eb9f0a7cd627c3dea5cf7643c8780821b9
Alexandru Panturu <alexandru.panturu@emag.ro>
2016-09-04 01:05:55 +0300

    add product reponse

--- services/responseFactory.js
+++ services/responseFactory.js
@@ -14,6 +14,9 @@ function ResponseFactory() {
 ResponseFactory.prototype = {
     getResponse: function (message) {
         this.loadPayload(message);
+        console.info(message);
+        console.info(Buffer.from(message,'base64').toString('ascii'));
+        
         if (message === MENU) {
             responsePayload.setType(MENU);
         }
