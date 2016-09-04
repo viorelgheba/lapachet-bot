690861edf2fc0d6225e287ea08634948d07e5b62
Alexandru Panturu <alexandru.panturu@emag.ro>
2016-09-04 09:56:56 +0300

    add notifier

--- services/response/finishOrderResponse.js
+++ services/response/finishOrderResponse.js
@@ -11,28 +11,28 @@ FinishOrderResponse.prototype = {
         var order = apiService.registerOrder(productId, intervalId, userId);
         console.log("Saved order: ", JSON.stringify(order));
         var response = {
-                attachment: {
-                    type: "template",
-                    payload: {
-                        template_type: "receipt",
-                        recipient_name: userId.toString(),
-                        order_number: order.orderId,
-                        currency: "USD",
-                        payment_method: "Visa 2345",
-                        order_url: "http://petersapparel.parseapp.com/order?order_id=123456",
-                        timestamp: "1428444852",
-                        elements: [
-                            {
-                                "title": product.name,
-                                "subtitle": product.description,
-                                "quantity": 1,
-                                "price": product.price,
-                                "currency": "RON",
-                                "image_url": product.image
-                            }
-                        ]
-                    }
+            attachment: {
+                type: "template",
+                payload: {
+                    template_type: "receipt",
+                    recipient_name: userId.toString(),
+                    order_number: order.orderId.toString(),
+                    currency: "USD",
+                    payment_method: "Visa 2345",
+                    order_url: "http://petersapparel.parseapp.com/order?order_id=123456",
+                    timestamp: "1428444852",
+                    elements: [
+                        {
+                            "title": product.name,
+                            "subtitle": product.description,
+                            "quantity": 1,
+                            "price": product.price == undefined ? 0 : product.price,
+                            "currency": "RON",
+                            "image_url": product.image
+                        }
+                    ]
                 }
+            }
         };
 
         return JSON.stringify(response);
