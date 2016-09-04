139712381ce6ebffbdb182df0306cde112b62ffb
Alexandru Panturu <alexandru.panturu@emag.ro>
2016-09-04 07:55:39 +0300

    add notifier

--- routes/notifier.js
+++ routes/notifier.js
@@ -5,8 +5,11 @@ var router = express.Router();
 var apiService = require('../services/apiService').getInstance();
 
 router.post('/', function (req, res) {
-    console.log(JSON.stringify(req));
-    res.send("Ok");
+    console.log(req);
+    var access_token = req.query['access_token'];
+    if (access_token != process.env.API_TOKEN) {
+        res.send("Invalid token!");
+    }
 });
 
 module.exports = router;
