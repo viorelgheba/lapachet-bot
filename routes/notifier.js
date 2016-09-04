02d16423ee92e7d3cd05cfce0f5b245a6712ce45
Alexandru Panturu <alexandru.panturu@emag.ro>
2016-09-04 08:00:01 +0300

    add notifier

--- routes/notifier.js
+++ routes/notifier.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var router = express.Router();
 var apiService = require('../services/apiService').getInstance();
+var facebookApi = require('../services/facebookService').getInstance();
 
 router.post('/', function (req, res) {
     console.log(req);
@@ -10,7 +11,13 @@ router.post('/', function (req, res) {
     if (access_token != process.env.API_TOKEN) {
         res.send("Invalid token!");
     }
-    res.send("Ok");
+    var users = req.query['users'];
+
+    users.forEach(function (user) {
+        console.log("Sending Message to userId: " + user);
+
+        facebookApi.sendMessage(user, "We got food suply :), please use this bot Menu");
+    })
 });
 
 module.exports = router;
